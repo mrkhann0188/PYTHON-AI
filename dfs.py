a81ce2da2b02ee3cdd1626527820c8c97576de4b
mrkhann0188 <your@email.com>
2025-08-19 11:13:05 +0530

    Add DFS implementation with author info

---
+++ dfs.py
@@ -0,0 +1,17 @@
+def dfs(graph,node,visited = None):
+    if visited is None:
+        visited = set()
+    visited.add(node)
+    print(node,end ='')
+    for neighbour in graph[node]:
+        if neighbour not in visited:
+            dfs(graph, neighbour,visited)
+graph ={
+    'A':['B','C'],
+    'B':['D','E'],
+    'C':['F'],
+    'D':[],
+    'E':['F'],
+    'F':[]
+}
+dfs(graph,'A')
