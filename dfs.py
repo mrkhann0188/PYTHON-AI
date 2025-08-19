555dfe1464507bfa2216f0609b4bb5da989f842a
mrkhann0188 <your@email.com>
2025-08-19 11:24:24 +0530

    Add BFS implementation

--- dfs.py
+++ dfs.py
@@ -1,17 +1,41 @@
-def dfs(graph,node,visited = None):
-    if visited is None:
-        visited = set()
-    visited.add(node)
-    print(node,end ='')
-    for neighbour in graph[node]:
-        if neighbour not in visited:
-            dfs(graph, neighbour,visited)
-graph ={
-    'A':['B','C'],
-    'B':['D','E'],
-    'C':['F'],
-    'D':[],
-    'E':['F'],
-    'F':[]
+from collections import deque
+
+# def dfs(graph,node,visited = None):
+#     if visited is None:
+#         visited = set()
+#     visited.add(node)
+#     print(node,end ='')
+#     for neighbour in graph[node]:
+#         if neighbour not in visited:
+#             dfs(graph, neighbour,visited)
+# graph ={
+#     'A':['B','C'],
+#     'B':['D','E'],
+#     'C':['F'],
+#     'D':[],
+#     'E':['F'],
+#     'F':[]
+# 
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+    while queue:
+        node = queue.popleft()
+        print(node, end=' ')
+        for neighbour in graph[node]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
+    print()
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
 }
-dfs(graph,'A')
+
+bfs(graph, 'A')
